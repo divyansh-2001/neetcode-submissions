3aff8b2ea5babfa35107c4837c81a3ba03f1593d
Divyansh Mehrotra <66470901+divyansh-2001@users.noreply.github.com>
2026-05-02 00:23:57 +0530

    Add: find-target-in-rotated-sorted-array - submission-8

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-8.cpp
@@ -0,0 +1,65 @@
+class Solution {
+private:
+    int getPivot(vector<int>& nums){
+        int start = 0, end = nums.size()-1;
+        while(start <= end){
+            if(start == end){           // length 1
+                return start;
+            }
+
+            if(end == start + 1){       // length 2
+                return nums[end] < nums[start] ? end : start;
+            }
+
+            int mid = start + (end-start)/2;
+
+            if(mid > 0){        // length of 3 atleast
+                if(nums[mid] < nums[mid-1]){
+                    return mid;
+                }
+
+                if(nums[mid] > nums[end]){
+                    start = mid + 1;
+                } else { 
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    int find(int start, int end, vector<int>& nums, int target){
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[mid] > target){
+                end = mid - 1;
+            } else { 
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size()-1;
+
+        int pivot = getPivot(nums);
+
+        if(pivot == 0){
+            return find(0, nums.size()-1, nums, target);
+        }
+
+        if(target >= nums[0] && target <= nums[pivot-1]){
+            return find(0, pivot-1, nums, target);
+        }
+
+        return find(pivot, nums.size()-1, nums, target);
+    }
+};
