72177f45b2a26beda11e49ed640f85464b129fb2
Divyansh Mehrotra <66470901+divyansh-2001@users.noreply.github.com>
2026-05-19 09:42:49 +0530

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+    void traverse(TreeNode* root, int depth, int pos, unordered_map<int, pair<int,int>>& m, int& depthMax){
+        if(!root){
+            return;
+        }
+
+        if(m.find(depth) == m.end() || m[depth].first <= pos){
+            m[depth] = {pos, root->val};
+        }
+
+        depthMax = max(depth, depthMax);
+
+        traverse(root->left, depth+1, pos-1, m, depthMax);
+        traverse(root->right, depth+1, pos+1, m, depthMax);
+
+        return;
+    }
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        unordered_map<int, pair<int,int>> m;
+        vector<int> v;
+        int depthMax = -1;
+
+        traverse(root, 0, 0, m, depthMax);
+
+        for(int i=0;i<=depthMax;i++){
+            v.push_back(m[i].second);
+        }
+
+        return v;
+    }
+};
+
+
+    //     1
+    // 2       3
+    //    5 6
