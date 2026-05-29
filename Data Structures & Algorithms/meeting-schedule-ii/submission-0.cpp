62a850349a33d815f832caeea7e7e50b44ad7181
Divyansh Mehrotra <66470901+divyansh-2001@users.noreply.github.com>
2026-05-29 09:05:38 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+struct comp{
+    bool operator()(Interval& a, Interval& b){
+        return a.start == b.start ? a.end < b.end : a.start < b.start;
+    }
+};
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if(intervals.size() <= 1){
+            return intervals.size();
+        }
+
+        sort(intervals.begin(), intervals.end(), comp());
+        int ans = 1;
+
+        priority_queue<int, vector<int>, greater<>> pq;
+        
+        pq.push(intervals[0].end);
+
+        for(int i=1;i<intervals.size();i++){
+            int currStart = intervals[i].start;
+            int currEnd = intervals[i].end;
+
+            if(currStart >= pq.top()){
+                pq.pop();
+            } else {
+                ans++;
+            }
+
+            pq.push(currEnd);
+        }
+
+        return ans;
+    }
+};
