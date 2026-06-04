0657b9c30265d7830c14e9b46dee11f244951241
Divyansh Mehrotra <66470901+divyansh-2001@users.noreply.github.com>
2026-06-04 22:49:28 +0530

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.cpp
@@ -0,0 +1,62 @@
+class Solution {
+private:
+    bool isValid(int x, int y, int m, int n){
+        return x >=0 && x < m && y >= 0 && y < n;
+    }
+
+    int arr[4][2] = {{0,1},{1,0},{-1,0},{0,-1}};
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int,int>> q;
+        int fresh = 0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 1){
+                    fresh++;
+                } else if(grid[i][j] == 2){
+                    q.push({i,j});
+                }
+            }
+        }   
+
+        q.push({-1, -1});
+        int t = 1;
+        bool consecBreak = false;
+
+        while(!q.empty()){
+            auto top = q.front();
+            q.pop();
+
+            if(top.first == -1){
+                if(consecBreak){
+                    break;
+                }
+
+                consecBreak = true;
+                q.push({-1, -1});
+                t++;
+
+                continue;
+            }
+
+            consecBreak = false;
+
+            for(int i=0;i<4;i++){
+                int dx = top.first + arr[i][0];
+                int dy = top.second + arr[i][1];
+
+                if(!isValid(dx, dy, m, n) || grid[dx][dy] != 1){
+                    continue;
+                }
+
+                grid[dx][dy] = 2;
+                fresh--;
+                q.push({dx, dy});
+            }
+        }
+        return fresh == 0 ? t-2: -1;
+    }
+};
